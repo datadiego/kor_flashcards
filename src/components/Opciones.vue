de4be2823736c1a85702f2f6b83e47653fc9e904
Juan Diego <juandiegomariscal@gmail.com>
2023-01-01 22:08:50 +0100

    FEAT 👍cargamos las opciones

--- src/components/Opciones.vue
+++ src/components/Opciones.vue
@@ -1,16 +1,35 @@
 <template>
     <div>
         <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
+            <li
+            v-if="tipo_ronda == 'caracter_significado'"
+            v-for="opcion in opciones_ronda"
+            :key="opcion.id"
+            >{{opcion.significado}}</li>
+            
+            <li
+            v-if="tipo_ronda == 'significado_caracter'"
+            v-for="opcion in opciones_ronda"
+            :key="opcion.id"
+            >{{opcion.caracter}}</li>
         </ul>
     </div>
 </template>
 
 <script>
+export default {
+    props:{
+        opciones_ronda:{
+            type: Array,
+            required: true
+        },
+        tipo_ronda:{
+            type: String,
+            required: true
+        }
 
+    }
+}
 </script>
 
 <style>
