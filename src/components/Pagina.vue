dc63fc1480b0f8fbbfe9cf0150cad06ee0c1fff5
Juan Diego <juandiegomariscal@gmail.com>
2023-01-02 12:30:51 +0100

    NEW 💯 añadimos el componente Puntos

--- src/components/Pagina.vue
+++ src/components/Pagina.vue
@@ -3,8 +3,9 @@
         <h1>🥬 Kimchi Cards 🥬</h1>
         <Selector @leccion-cambiada="actualizarLeccion" />
         <div v-if="leccion_cargada">
-            <h2 v-if="tipo_ronda_actual=='caracter_significado'">{{ pregunta_actual.caracter }}</h2>
-            <h2 v-else>{{ pregunta_actual.significado }}</h2>
+            <Puntos :aciertos="aciertos" :fallos="fallos" />
+            <h2 v-if="tipo_ronda_actual=='caracter_significado'" id="pregunta_actual">{{ pregunta_actual.caracter }}</h2>
+            <h2 v-else id="pregunta_actual">{{ pregunta_actual.significado }}</h2>
         </div>
         <div v-if="!leccion_cargada">
             <h2>Selecciona una lección para empezar</h2>
@@ -17,6 +18,7 @@
 <script>
 import Selector from './Selector.vue'
 import Opciones from './Opciones.vue'
+import Puntos from './Puntos.vue'
 
 import leccion1 from '../assets/coreano1.csv'
 import leccion2 from '../assets/coreano2.csv'
@@ -31,6 +33,8 @@ export default {
             leccion_cargada: false,
             tipos_ronda: ['caracter_significado','significado_caracter'],
             tipo_ronda_actual: null,
+            aciertos: 0,
+            fallos: 0
             
         }
     },
@@ -56,7 +60,8 @@ export default {
 },
     components: {
         Selector,
-        Opciones
+        Opciones,
+        Puntos
     },
     beforeMount(){
         const randint = Math.floor(Math.random() * this.tipos_ronda.length)
@@ -74,5 +79,13 @@ export default {
     height: 100vh;
     width: 100vw;
     background-color: #f5f5f5;
+    text-align: center;
+}
+
+#pregunta_actual{
+    background-color: aquamarine;
+    font-size: 50px;
+    border-radius: 30px;
+    padding: 20px;
 }
 </style>
